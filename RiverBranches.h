525a1cca7e945a4059d7a6be91f51cae1aecac9c
Jie Meng <jiemeng@seas.upenn.edu>
2018-03-16 15:14:03 -0400

    init repo

---
+++ RiverBranches.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "vec.h"
+#include <vector>
+
+// class of river node
+class RiverNode
+{
+public:
+	// node priority, used for node creation
+	int priority;
+	// node position
+	vec3 position;
+	// parent node
+	RiverNode* parent;
+	// children nodes
+	vector<RiverNode*> children;
+	//Terminal nodes or not
+	bool terminal;
+
+	RiverNode();
+	RiverNode(int p, vec3 pos, RiverNode* parent);
+	~RiverNode();
+
+	// setters and getters
+	//void updatePriority(int p);
+	//int getPriority();
+	//void getPosition(vec3 pos);
+	//vec3 getPosition();
+	//void setParent(RiverNode* p);
+	//RiverNode* getParent();
+	//void addChild(RiverNode* c);
+	//RiverNode* getChild(int i);
+};
+
+
+
+// class of river branch
+class RiverBranch
+{
+public:
+	RiverNode* start;
+	RiverNode* end;
+
+	static int id;
+
+	RiverBranch(RiverNode* s, RiverNode* e);
+	double distance(RiverBranch* branch);
+};
---
+++ RiverNetwork.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include "RiverBranches.h"
+#include <vector>
+
+// class of cell
+class Cell
+{
+	RiverNode* center;
+	vector<RiverNode*> corners;
+
+public:
+
+	Cell();
+	~Cell();
+	float getArea();
+};
+
+
+// class of river network
+class RiverNetwork
+{
+public:
+	int width, height;
+
+	double e;
+
+	int numW, numH;
+
+	vector<RiverNode*> nodes;
+	vector<RiverBranch*> branches;
+
+	vector<vector<RiverBranch*>> grids;
+	//this vector stores all the non-terminal nodes that currently can be expanded
+	vector<RiverNode*> nonTerminalNodes;
+	vector<vector<double>> elevationMap;
+
+	//current maximum priority
+	int maxP;
+	//current minimum elevation
+	double minElevation;
+
+	//RiverNetwork();
+	RiverNetwork(int w = 0, int h = 0, double e = 0.0);
+	~RiverNetwork();
+
+
+	//create the nodes to start from
+	void initialNode();
+	//select candidate nodes for expansion
+	RiverNode* selectNode(double elevationRange);
+	//node expansion operation
+	void expandNode(RiverNode* node);
+	//get next candidate node
+	RiverNode* getCandidate(RiverNode* parent, double angle, int p);
+	//validation of the new node
+	bool validateNode(RiverNode* node, double boundary, RiverBranch* branch);
+
+};
